d5e64fb50f58ca1769ea376b3765bc4bc772493d
Larissa Rabelo <56235344+larissarabelo@users.noreply.github.com>
2022-01-26 13:27:45 -0300

    Update half tone higher.py

--- half tone higher.py
+++ half tone higher.py
@@ -3,7 +3,7 @@ import librosa
 import IPython.display as ipd
 #half tone higher
 from IPython.display import Audio
-y, sr=librosa.load("PARTICIPANTE_9.wav", sr=None)
+y, sr=librosa.load("audio.wav", sr=None)
 wav_pitch_sf = librosa.effects.pitch_shift(y,sr,n_steps=1)
 #plot_spec(data=wav_pitch_sf,sr=sr,title=f'Pitch shifting by {-5} steps',fpath="C:/Users/larit/Desktop/Segurança da Informação/PIBIC/")
 ipd.Audio(wav_pitch_sf,rate=sr)
